2e3ae105c46f21f99dc4aeb1815a2c55e2eece77
Timhansen1990 <timx1776@edu.easj.dk>
2021-09-30 12:36:17 +0200

    Exercise2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,30 +9,36 @@ public class Main {
     static void printTile(Tile tile) {
         System.out.println(tile);
     }
+    // creating a method with the Tile parameter there is defined in the Tile.java file, and print the tile parameter.
 
     static void printDate(Date date){
         System.out.println(date);
     }
+    // creating PrintDate method and taking the parameter date and prints the date parameter.
 
     public static void main(String[] args) {
         Date birthday = new Date(2002, 6,6);
         Date date2 = new Date(2021, 9, 30);
+        // defined the birthday and date2 with values.
 
         Tile tile1 = new Tile(10, 'c');
         printTile(tile1);
+        // creating the tile1 and use the parameters required.
 
         Tile tile2 = new Tile(10, 'c');
+        // creating the tile2 and use the parameters required.
 
         Tile tile3 = new Tile(5, 'z');
-
+        // creating the tile3 and use the parameters required.
         System.out.println("tile1 equals tile 2: " + tile1.equals(tile2));
         System.out.println("tile1 equals tile 3: " + tile1.equals(tile3));
-
+        // printing the boolean expression if tile1 is equal to 2 and if tile1 is equal to 3.
         char letter = tile3.getLetter();
         tile3.setLetter('w');
+        // creating the char letter and get and set letter to a value
 
         printDate(date2);
         printDate(birthday);
-
+        // printing date2 and birthday.
     }
 }
--- src/com/company/Tile.java
+++ src/com/company/Tile.java
@@ -4,32 +4,41 @@ public class Tile {
     private int value;
     private char letter;
 
+    // creating the class Tile.
+
     Tile(int value, char letter) {
         this.value = value;
         this.letter = letter;
     }
+    // creating the constructor method that takes the parameter value and letter.
 
     public int getValue() {
         return value;
     }
+    // using the getValue method to store the value of the value parameter.
 
     public void getValue(int value) {
         this.value = value;
     }
+    // taking the value and store it in this.value
 
     public char getLetter() {
         return letter;
     }
+    // using the getLetter method to declare we have to return letter.
 
     public void setLetter(char letter) {
         this.letter = letter;
     }
+    // here we set the letter to a parameter defined in the main method.
 
     public String toString() {
         return "Tile: " + value + " Letter: " + letter;
     }
+    // Here we use the toString method to make sure we get the value and not the placement in the memory.
 
     public boolean equals(Tile that) {
         return value == that.value && letter == that.letter;
     }
+    // here we defined the equals' method, so we can compare a string with an int.
 }
